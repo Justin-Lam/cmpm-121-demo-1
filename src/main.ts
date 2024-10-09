51a5edd5610d4e42628a538fd28344d59b13fe4a
Justin-Lam <justinflam9225k@gmail.com>
2024-10-08 22:05:03 -0700

    Modified comments

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
-// Button
+// Fry Button
 const button = document.createElement("button");
 button.innerHTML = "🍟 make a french fry 🍟";
 app.append(button);
