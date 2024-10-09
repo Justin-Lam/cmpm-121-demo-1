43259d451d4fa9db941740f6383f746a48d5a077
Justin-Lam <justinflam9225k@gmail.com>
2024-10-08 21:55:10 -0700

    Added a button

--- src/main.ts
+++ src/main.ts
@@ -8,3 +8,9 @@ document.title = gameName;
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+
+// Button
+const button = document.createElement("button");
+button.innerHTML = "Press me";
+app.append(button);
