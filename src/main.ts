856b34c39a9745f6166ebcb7494b039b4fb4c4ae
Justin-Lam <justinflam9225k@gmail.com>
2024-10-08 22:20:17 -0700

    Gave counter fun label; Made fry button increase counter; Step 2 complete

--- src/main.ts
+++ src/main.ts
@@ -9,12 +9,19 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
+// Variables
+let numFries: number = 0;
+
 // Fry Button
 const button = document.createElement("button");
 button.innerHTML = "🍟 make a french fry 🍟";
+button.addEventListener("click", () => {
+	numFries++;
+	counter.innerHTML = `you have ${numFries} frenchy fries`;
+});
 app.append(button);
 
 // Fry Counter
 const counter = document.createElement("div");
-counter.innerHTML = "you have " + 10 + " fries";
+counter.innerHTML = `you have ${numFries} frenchy fries`;
 app.append(counter);
