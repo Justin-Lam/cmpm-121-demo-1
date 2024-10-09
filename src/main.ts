6ec1a7cc47bf0b35c2cbb35fd4c1a3b3ba8cece2
Justin-Lam <justinflam9225k@gmail.com>
2024-10-08 22:49:47 -0700

    Removed the setInterval based auto clicker and implemented a requestAnimationFrame loop based one that's currently broken

--- src/main.ts
+++ src/main.ts
@@ -9,10 +9,10 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
-
 // Variables
 let numFries: number = 0;
-
+let previousTime: DOMHighResTimeStamp = performance.now();
+const autoClickerSecondsPerClick: number = 1;
 
 // Fry Button
 const button = document.createElement("button");
@@ -20,19 +20,25 @@ button.innerHTML = "🍟 make a french fry 🍟";
 button.addEventListener("click", () => makeFries(1));
 app.append(button);
 
-
 // Fry Counter
 const counter = document.createElement("div");
 counter.innerHTML = `you have ${numFries} frenchy fries`;
 app.append(counter);
 
-
 // Auto Clicker
-setInterval(makeFries, 1000, 1);
-
+requestAnimationFrame(autoClickerLoop);
 
 // Functions
 function makeFries(amount: number): void {
-	numFries += amount;
-	counter.innerHTML = `you have ${numFries} frenchy fries`;
+  numFries += amount;
+  counter.innerHTML = `you have ${numFries} frenchy fries`;
+}
+
+function autoClickerLoop(currentTime: DOMHighResTimeStamp): void {
+	const deltaTime = currentTime - previousTime;
+	if (deltaTime >= autoClickerSecondsPerClick) {
+		makeFries(1);
+		previousTime = currentTime;
+	}
+	requestAnimationFrame(autoClickerLoop);
 }
